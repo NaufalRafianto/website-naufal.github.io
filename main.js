7a8f79beb96bf068032f3109aa590c33cc11df6f
NaufalRafianto <naufl.rafianto21@gmail.com>
2022-03-15 14:36:00 +0700

    initial commit

---
+++ main.js
@@ -0,0 +1,25 @@
+// About
+const tabsContainer = document.querySelector('.about-tabs'),
+aboutSection = document.querySelector('.about-section')
+
+tabsContainer. addEventListener("click", (e) =>{
+    if(e.target.classList.contains("tab-item") && !e.target.classList.contains('active')){
+        tabsContainer.querySelector('.active').classList.remove('active');
+        e.target.classList.add('active');
+        const target = e.target.getAttribute('data-target');
+        aboutSection.querySelector('.tab-content.active').classList.remove('active');
+        aboutSection.querySelector(target).classList.add('active');
+    }
+}
+)
+
+
+// Navbar
+const navToggler = document.querySelector('.nav-toggler');
+navToggler.addEventListener('click', () => {
+    hideSection();
+});
+
+function hideSection(){
+    document.querySelector('section.active').classList.toggle('fade-out');
+}
